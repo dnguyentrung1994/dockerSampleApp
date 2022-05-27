d9944687a21ae44ca7a4076a5b7c14c65d814431
dnguyentrung1994 <msmsms4656@gmail.com>
2022-05-27 17:08:59 +0900

    fix: navbar

--- client/components/TheNavbar.vue
+++ client/components/TheNavbar.vue
@@ -3,41 +3,41 @@
     <div
       class="sticky flex top-0 min-w-full bg-black align-middle z-40 h-10 justify-between"
     >
-      <div class="navbar-item">
-        <button
-          variant="primary"
-          class="bg-black text-white font-normal py-2 px-4 rounded inline-flex items-center hover:bg-gray-600 transition-all"
-          :class="displaySidebar ? buttonSizeFull : buttonSizeReduced"
-          @click="toggleSidebar"
+      <button
+        variant="primary"
+        class="bg-black text-white font-normal py-2 px-4 rounded inline-flex items-center hover:bg-gray-600 transition-all"
+        :class="displaySidebar ? buttonSizeFull : buttonSizeReduced"
+        @click="toggleSidebar"
+      >
+        <svg
+          class="w-4 h-4 mr-2"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
         >
-          <svg
-            class="w-4 h-4 mr-2"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <rect width="20px" height="3px" rx="1px" class="fill-white" />
-            <rect
-              y="8px"
-              width="20px"
-              height="3px"
-              rx="1px"
-              class="fill-white"
-            />
-            <rect
-              y="16px"
-              width="20px"
-              height="3px"
-              rx="1px"
-              class="fill-white"
-            />
-          </svg>
-          <span :hidden="!displaySidebar">Placeholder</span>
-        </button>
-      </div>
+          <rect width="20px" height="3px" rx="1px" class="fill-white" />
+          <rect y="8px" width="20px" height="3px" rx="1px" class="fill-white" />
+          <rect
+            y="16px"
+            width="20px"
+            height="3px"
+            rx="1px"
+            class="fill-white"
+          />
+        </svg>
+        <span :hidden="!displaySidebar">Placeholder</span>
+      </button>
+
       <o-field class="max-w-[500px] space-x-8">
         <o-input placeholder="Search..." expanded></o-input>
         <o-button variant="primary">Search</o-button>
       </o-field>
+
+      <button
+        variant="primary"
+        class="bg-black text-white font-normal py-2 px-4 rounded inline-flex items-center hover:bg-gray-600 transition-all"
+      >
+        <span>Login</span>
+      </button>
     </div>
     <TheSidebar :displaySidebar="!displaySidebar" />
   </div>
