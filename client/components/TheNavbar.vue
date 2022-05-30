292a3c14390e2b4906c9e6c33bfab0feb24b079c
dnguyentrung1994 <msmsms4656@gmail.com>
2022-05-30 17:52:26 +0900

    add: login form

--- client/components/TheNavbar.vue
+++ client/components/TheNavbar.vue
@@ -32,34 +32,82 @@
         <o-button variant="primary">Search</o-button>
       </o-field>
 
-      <button
-        variant="primary"
-        class="bg-black text-white font-normal py-2 px-4 rounded inline-flex items-center hover:bg-gray-600 transition-all"
-      >
-        <span>Login</span>
-      </button>
+      <div class="flex flex-row">
+        <button
+          variant="primary"
+          class="bg-black text-white font-normal py-2 px-4 inline-flex items-center hover:bg-gray-600 transition-all pr-4 border-r-[1px] border-solid border-white"
+          @click="toggleLogin"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+            />
+          </svg>
+          <span>Login</span>
+        </button>
+
+        <button
+          variant="primary"
+          class="bg-black text-white font-normal py-2 px-4 inline-flex items-center hover:bg-gray-600 transition-all pl-4 border-r-2 border-solid border-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
+            />
+          </svg>
+          <span>Register</span>
+        </button>
+      </div>
     </div>
     <TheSidebar :displaySidebar="!displaySidebar" />
+    <the-login-form v-show="loginClicked" @closeLoginModal="toggleLogin" />
   </div>
 </template>
 
 <script>
+import TheLoginForm from "./TheLoginForm.vue";
 export default {
+  components: { TheLoginForm },
   data: () => {
     return {
       displaySidebar: false,
       buttonSizeReduced: "w-20",
       buttonSizeFull: "w-[260px]",
+      loginClicked: false,
     };
   },
   watch: {
     displaySidebar() {
       console.log(this.displaySidebar);
     },
+    loginClicked() {
+      console.log(this.loginClicked);
+    },
   },
   methods: {
     toggleSidebar() {
       this.displaySidebar = !this.displaySidebar;
+    },
+    toggleLogin() {
+      this.loginClicked = !this.loginClicked;
     },
   },
 };
--- client/components/TheSidebar.vue
+++ client/components/TheSidebar.vue
@@ -7,13 +7,28 @@
         open
         :reduce="reduce"
         :mobile="mobile"
+        :class="reduce ? classReduced : classFull"
       >
-        <section class="flex pt-20">
+        <section class="flex pt-10">
           <div
-            class="bg-inherit hover:bg-white text-black font-normal inline-flex"
+            class="bg-inherit hover:bg-white text-black font-normal flex flex-row w-full transition-all h-[30px] pl-4"
           >
-            <NuxtLink to="/">
-              <o-icon pack="fas" icon="home"> HOME </o-icon>
+            <NuxtLink to="/" class="flex flex-row">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              <span v-if="!reduce" class="pl-5">Home</span>
             </NuxtLink>
           </div>
         </section>
@@ -30,6 +45,8 @@ export default {
       iconSizeStyle: "w-6 h-6",
       iconMargin: "mr-4",
       mobile: "reduced",
+      classReduced: "max-w-[40px]",
+      classFull: "max-w-[160px]",
     };
   },
   props: ["displaySidebar"],
